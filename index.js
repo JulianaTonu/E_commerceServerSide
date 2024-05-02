7f3ab850de96ce1ef9a3ec9be9a67bb2da02f726
julianatonu <julianatonu@gmail.com>
2024-05-02 11:58:02 +0600

    deleteCart

--- index.js
+++ index.js
@@ -64,6 +64,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('id', id);
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
