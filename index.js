86c79f247eba992eeeae3b0590f2955ebededf3f
julianatonu <julianatonu@gmail.com>
2024-05-07 14:18:37 +0600

    verifyAdmin

--- index.js
+++ index.js
@@ -54,7 +54,17 @@ async function run() {
       })
     }
 
-
+//verifyAdmin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next();
+    }
 
     //User Collections
     app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
